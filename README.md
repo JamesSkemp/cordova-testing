131e73c11486b54cfe84da3b5da76a4f42659d2a
James Skemp <strivinglife@gmail.com>
2016-03-07 20:54:22 -0600

    Added Phaser to example project.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cordova-testing
 Testing with Apache Cordova.
+
+Sort of based upon http://taco.visualstudio.com/en-us/docs/get-started-first-mobile-app/
--- SimplePhaserTest/www/scripts/index.js
+++ SimplePhaserTest/www/scripts/index.js
@@ -3,26 +3,37 @@
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
 (function () {
-    "use strict";
+	"use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var element = document.getElementById("deviceready");
-        element.innerHTML = 'Device Ready';
-        element.className += ' ready';
-    };
+	function onDeviceReady() {
+		// Handle the Cordova pause and resume events
+		document.addEventListener('pause', onPause.bind(this), false);
+		document.addEventListener('resume', onResume.bind(this), false);
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+		// TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+		var element = document.getElementById("deviceready");
+		element.innerHTML = 'Device Ready';
+		element.className += ' ready';
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+		var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create })
+		alert("Phaser Going Called ...");
+	};
+
+	function preload() {
+
+	};
+
+	function create() {
+
+	};
+
+	function onPause() {
+		// TODO: This application has been suspended. Save application state here.
+	};
+
+	function onResume() {
+		// TODO: This application has been reactivated. Restore application state here.
+	};
+})();
