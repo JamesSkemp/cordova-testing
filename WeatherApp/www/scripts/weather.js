973122d5194b7c3665efeb5f38baf9941629113f
James Skemp <strivinglife@gmail.com>
2016-03-09 20:18:49 -0600

    Dumped in JS to use geolocation data.

--- WeatherApp/www/scripts/weather.js
+++ WeatherApp/www/scripts/weather.js
@@ -45,4 +45,41 @@
 		return false;
 	}
 
+	ns.getLocation = function () {
+		navigator.geolocation.getCurrentPosition(onGetLocationSuccess, onGetLocationError, { enableHighAccuracy: true });
+
+		$('#error-msg').show();
+		$('#error-msg').text('Determining your current location ...');
+
+		$('#get-weather-btn').prop('disabled', true);
+	}
+
+	var onGetLocationSuccess = function (position) {
+		var latitude = position.coords.latitude;
+		var longitude = position.coords.longitude;
+
+		// get zip code by using latitude and longitude.
+		var queryString = 'http://gws2.maps.yahoo.com/findlocation?pf=1&locale=en_US&offset=15&flags=&q='
+			+ latitude + '%2c' + longitude + '&gflags=R&start=0&format=json';
+
+		$.getJSON(queryString, function (results) {
+			$('#error-msg').hide();
+
+			if (results.Found > 0) {
+				// put the zip code into the input box for the user if we get a location
+				var zipCode = results.Result.uzip
+				$('#zip-code-input').val(zipCode);
+			}
+		}).fail(function () {
+			$('#error-msg').text('Error retrieving data.');
+		}).always(function () {
+			// always reset the UI even if we fail to get a ZIP code from the service.
+			$('#get-weather-btn').prop('disabled', false);
+		});
+	}
+
+	var onGetLocationError = function (error) {
+		$('#error-msg').text('Error getting location. Leaving zip code field blank');
+		$('#get-weather-btn').prop('disabled', false);
+	}
 })($, WeatherApp, navigator);
